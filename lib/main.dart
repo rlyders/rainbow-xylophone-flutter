dec5e0674e0bb44e68412747a3918aa88d6e2ccc
RJLyders <richard@lyders.com>
2019-09-29 13:20:09 -0500

    added classes, lists, and for-loops to reduce and reuse

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
+
+playSound(int soundNumber) {
+  final player = AudioCache();
+  player.play('note$soundNumber.wav');
+}
+
+class XylophoneBar extends Expanded {
+  XylophoneBar(Color color, int note)
+      : super(
+          child: FlatButton(
+            color: color,
+            onPressed: () {
+              playSound(note);
+            },
+          ),
+        );
+}
+
+class RainbowXylophone extends Column {
+  static final List<Color> rainbowColors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.indigo,
+    Colors.purple
+  ];
+
+  static List<XylophoneBar> getRainbowXylophoneBars() {
+    List<XylophoneBar> rainbowBars = [];
+    rainbowColors.asMap().forEach((index, color) {
+      XylophoneBar xylophoneBar = XylophoneBar(color, index);
+      rainbowBars.add(xylophoneBar);
+    });
+    return rainbowBars;
+  }
+
+  RainbowXylophone()
+      : super(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: getRainbowXylophoneBars(),
+        );
+}
 
 class XylophoneApp extends StatelessWidget {
   @override
@@ -8,7 +54,7 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: RainbowXylophone(),
         ),
       ),
     );
